785d717b5e2b9b45459d85ef1760e7a73e52d907
Florian Knoop <florian.knoop@liu.se>
2023-08-15 14:15:19 +0200

    tests | add thermal_conductivity

---
+++ tests/thermal_conductivity/test_thermal_conductivity.py
@@ -0,0 +1,39 @@
+import numpy as np
+import xarray as xr
+from pathlib import Path
+
+parent = Path(__file__).parent
+folder = parent / "reference"
+
+files_hdf5 = [
+    "outfile.cumulative_kappa.hdf5",
+]
+
+
+def test_mean_square_displacement(file="outfile.thermal_conductivity"):
+    file_ref = folder / file
+    file_new = parent / file
+
+    data_ref = np.loadtxt(file_ref)
+    data_new = np.loadtxt(file_new)
+
+    assert np.allclose(data_ref, data_new), f"CHECK {file_new.absolut()}"
+
+
+def test_hdf5(files=files_hdf5):
+    for file in files:
+        file_ref = folder / file
+        file_new = parent / file
+
+        ds_ref = xr.load_dataset(file_ref)
+        ds_new = xr.load_dataset(file_new)
+
+        for var in ds_ref.data_vars:
+            x = ds_ref[var]
+            y = ds_new[var]
+            assert np.allclose(x, y), file_new
+
+
+if __name__ == "__main__":
+    test_mean_square_displacement()
+    test_hdf5()
