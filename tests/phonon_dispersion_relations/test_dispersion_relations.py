a1a14726c31122e9696709755d06eb52a3b697d0
Florian Knoop <florian.knoop@liu.se>
2023-08-15 15:47:47 +0200

    tests | update dispersion test

--- tests/phonon_dispersion_relations/test_dispersion_relations.py
+++ tests/phonon_dispersion_relations/test_dispersion_relations.py
@@ -1,28 +1,19 @@
 import numpy as np
-import xarray as xr
 from pathlib import Path
 
 parent = Path(__file__).parent
 folder = parent / "reference"
 
-files_hdf5 = [
-    "outfile.dispersion_relations.hdf5",
-]
 
+def test_dispersion(file="outfile.dispersion_relations"):
+    file_ref = folder / file
+    file_new = parent / file
 
-def test_hdf5(files=files_hdf5):
-    for file in files:
-        file_ref = folder / file
-        file_new = parent / file
+    data_ref = np.loadtxt(file_ref)
+    data_new = np.loadtxt(file_new)
 
-        ds_ref = xr.load_dataset(file_ref)
-        ds_new = xr.load_dataset(file_new)
-
-        for var in ds_ref.data_vars:
-            x = ds_ref[var]
-            y = ds_new[var]
-            assert np.allclose(x, y), file_new
+    assert np.allclose(data_ref, data_new), file_new.absolute()
 
 
 if __name__ == "__main__":
-    test_hdf5()
+    test_dispersion()
