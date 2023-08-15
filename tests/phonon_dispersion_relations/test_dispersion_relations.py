d1dfbedd58a3ab8f43fa49e9c6ab8bf2a17b50d8
Florian Knoop <florian.knoop@liu.se>
2023-08-15 14:09:05 +0200

    test | phonon_dispersion_relations

---
+++ tests/phonon_dispersion_relations/test_dispersion_relations.py
@@ -0,0 +1,28 @@
+import numpy as np
+import xarray as xr
+from pathlib import Path
+
+parent = Path(__file__).parent
+folder = parent / "reference"
+
+files_hdf5 = [
+    "outfile.dispersion_relations.hdf5",
+]
+
+
+def test_hdf5(files=files_hdf5):
+    for file in files:
+        file_ref = folder / file
+        file_new = parent / file
+
+        ds_ref = xr.load_dataset(file_ref)
+        ds_new = xr.load_dataset(file_new)
+
+        for var in ds_ref.data_vars:
+            x = ds_ref[var]
+            y = ds_new[var]
+            assert np.allclose(x, y), file_new
+
+
+if __name__ == "__main__":
+    test_hdf5()
