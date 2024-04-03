18066e68a1dce1095eef4cf2716365b0d2812df0
Matthieu Verstraete <matthieu.jean.verstraete@gmail.com>
2024-04-03 16:20:04 +0200

    np happy with filtered DDB format file

--- tests/dump_dynamical_matrices/test_dump_dynamical_matrices.py
+++ tests/dump_dynamical_matrices/test_dump_dynamical_matrices.py
@@ -2,15 +2,15 @@ import numpy as np
 from pathlib import Path
 
 parent = Path(__file__).parent
-folder = f"{parent}/reference"
+folder = parent / "reference"
 
 
 def test_dispersion(file="outfile.many_dynamical_matrices_DDB"):
-    file_ref = f"{folder}/{file}"
-    file_new = f"{parent}/{file}"
+    file_ref = folder / file
+    file_new = parent / file
 
-    data_ref = np.loadtxt(file_ref)
-    data_new = np.loadtxt(file_new)
+    data_ref = np.loadtxt(file_ref,skiprows=151,comments=["q","2n"])
+    data_new = np.loadtxt(file_new,skiprows=151,comments=["q","2n"])
 
     np.testing.assert_allclose(data_ref, data_new, err_msg=file_new.absolute())
     print ("all done and all close")
