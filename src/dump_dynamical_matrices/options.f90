d411c79a9681349495ac1abae31ffd0b4123f21f
Florian Knoop <florian.knoop@liu.se>
2024-04-05 09:31:51 +0200

    format

--- src/dump_dynamical_matrices/options.f90
+++ src/dump_dynamical_matrices/options.f90
@@ -11,8 +11,8 @@ type lo_opts
     integer :: meshtype
     !> read it from file instead
     logical :: readqpointsfromfile
-    contains
-        procedure :: parse
+contains
+    procedure :: parse
 end type
 
 contains
@@ -25,25 +25,24 @@ subroutine parse(opts)
     type(command_line_interface) :: cli
 
     ! basic stuff, for the help thingy
-    call cli%init(progname    = 'dump_dynamical_matrices',&
-              authors     = lo_author,&
-              version     = lo_version,&
-              license     = lo_licence,&
-              help        = 'Usage: ',&
-              description = 'Dump the dynamical matrices to file, either on a grid or at the points specified by a file.',&
-              epilog      = new_line('a')//"...")
+    call cli%init(progname='dump_dynamical_matrices', &
+                  authors=lo_author, &
+                  version=lo_version, &
+                  license=lo_licence, &
+                  help='Usage: ', &
+                  description='Dump the dynamical matrices to file, either on a grid or at the points specified by a file.', &
+                  epilog=new_line('a')//"...")
 
     cli_qpoint_grid
     cli_meshtype
     !
-    call cli%add(switch='--readqpoints',&
-            help='Instead of generating a q-mesh, read it in fractional coordinates from a file called "infile.dynmatqpoints"',&
-            required=.false.,act='store_true',def='.false.',error=lo_status)
-            if ( lo_status .ne. 0 ) stop
-    call cli%get(switch='--qpoint_grid',val=opts%qgrid)
-    call cli%get(switch='--meshtype',val=opts%meshtype)    
-    call cli%get(switch='--readqpoints',val=opts%readqpointsfromfile)    
+    call cli%add(switch='--readqpoints', &
+                 help='Instead of generating a q-mesh, read it in fractional coordinates from a file called "infile.dynmatqpoints"', &
+                 required=.false., act='store_true', def='.false.', error=lo_status)
+    if (lo_status .ne. 0) stop
+    call cli%get(switch='--qpoint_grid', val=opts%qgrid)
+    call cli%get(switch='--meshtype', val=opts%meshtype)
+    call cli%get(switch='--readqpoints', val=opts%readqpointsfromfile)
 end subroutine
-
 
 end module
--- tests/dump_dynamical_matrices/test_dump_dynamical_matrices.py
+++ tests/dump_dynamical_matrices/test_dump_dynamical_matrices.py
@@ -9,11 +9,11 @@ def test_dispersion(file="outfile.many_dynamical_matrices_DDB"):
     file_ref = folder / file
     file_new = parent / file
 
-    data_ref = np.loadtxt(file_ref,skiprows=151,comments=["q","2n"])
-    data_new = np.loadtxt(file_new,skiprows=151,comments=["q","2n"])
+    data_ref = np.loadtxt(file_ref, skiprows=151, comments=["q", "2n"])
+    data_new = np.loadtxt(file_new, skiprows=151, comments=["q", "2n"])
 
     np.testing.assert_allclose(data_ref, data_new, err_msg=file_new.absolute())
-    print ("all done and all close")
+    print("all done and all close")
 
 
 if __name__ == "__main__":
