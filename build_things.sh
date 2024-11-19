28cf52f37efe209a69496a5be93294bd069b1f79
Florian Knoop <florian.knoop@liu.se>
2024-11-19 19:42:23 +0100

    conda | fix

--- build_things.sh
+++ build_things.sh
@@ -244,7 +244,7 @@ do
     # link it to bin?
     # [ -f build/${code}/${code} ] && ln -sf ../build/${code}/${code} bin/${code}
     # move it instead
-    [ -f build/${code}/${code} ] && mv ../build/${code}/${code} bin/${code}
+    [ -f build/${code}/${code} ] && mv build/${code}/${code} bin/${code}
 done
 
 basedir=`pwd`
