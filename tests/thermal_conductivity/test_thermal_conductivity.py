7620cfb632b352453f346507c4fae9e4d17d29a1
Florian Knoop <florian.knoop@liu.se>
2024-12-05 11:28:34 +0100

    test | check if thermal conductivity output is consistent

--- tests/thermal_conductivity/test_thermal_conductivity.py
+++ tests/thermal_conductivity/test_thermal_conductivity.py
@@ -53,6 +53,24 @@ def test_hdf5(file, var, atol=1, rtol=0.01):
     np.testing.assert_allclose(x, y, atol=atol, rtol=rtol, err_msg=var)
 
 
+def test_conductivity_comparison(
+    file="outfile.thermal_conductivity",
+    file_grid="outfile.thermal_conductivity_grid.hdf5",
+):
+    """Check if the thermal conducivities conincide"""
+    data1 = np.loadtxt(file)
+
+    # without off-diagonal contribution
+    kappa1 = data1[0][:3] + data1[1][:3]
+
+    ds = xr.load_dataset(file_grid)
+
+    kappa2 = np.diag(ds.thermal_conductivity.sum(axis=(0, 1)) / ds.number_of_qpoints)
+
+    np.testing.assert_allclose(kappa1, kappa2)
+
+
 if __name__ == "__main__":
     test_thermal_conductivity()
     test_hdf5()
+    test_conductivity_comparison()
